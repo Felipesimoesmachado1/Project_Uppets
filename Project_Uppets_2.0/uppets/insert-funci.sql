1ec0ef6be998499076e2d536c0d3448e7ba25f15
Arthur de Lima Silveira <arthur_l_silveira@estudante.sesisenai.org.br>
2025-08-19 15:21:25 -0300

    Delete Project_Uppets_2.0/uppets directory

--- Project_Uppets_2.0/uppets/insert-funci.sql
+++
@@ -1,4 +0,0 @@
--- Inserção de funcionários exemplo (com senha em hash fictício)
-INSERT INTO funcionarios (nome, email, senha, cargo) VALUES
-('João', 'joao@clinica.com', '12345', 'atendente'),
-('Dra. Ana', 'ana@clinica.com', '1234', 'veterinario');
